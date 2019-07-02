db4dd8637a18fdf8d78bb479926b438beea74273
Sergey <svolchenkov@gmail.com>
2019-07-02 09:58:40 -0500

    first commit

--- src/main/java/com/example/demo/controller/FirstController.java
+++ src/main/java/com/example/demo/controller/FirstController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Controllers {
+public class FirstController {
 
     @GetMapping
     public  ResponseEntity<String> test() {
