f51a8784239c6f2cff0a07132f87324f4fa46f75
Sergey <svolchenkov@gmail.com>
2019-07-02 11:34:00 -0500

    added test to path

--- src/main/java/com/example/demo/controller/FirstController.java
+++ src/main/java/com/example/demo/controller/FirstController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FirstController {
 
-    @GetMapping
+    @GetMapping("/test")
     public  ResponseEntity<String> test() {
 
 
